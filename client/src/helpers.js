a68ff80c4bdc39fea2630eb70ae9d964427141ae
fy2be <36625528+fy2be@users.noreply.github.com>
2018-06-13 13:16:57 +0200

    add splitDivisions

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -38,4 +38,29 @@ function splitPlanets(stringWithPlanets) {
     return planets;
 }
 
-export { distance, splitPlanets };
+function splitDivisions(stringWithDivisions) {
+    // input:
+    // '2 test XXXX YYYY 10 test2 AAA ZZZ 20';
+
+    const regex = /[a-zA-Z0-9]+ [A-Z0-9]{4,} [A-Z0-9]{4,} \d+/g;
+
+    const divisions = [];
+    let result;
+    do {
+        result = regex.exec(stringWithDivisions);
+        if (result) {
+            const [name, from, to, ships] = result[0].split(' ');
+            divisions.push({
+                name,
+                from,
+                to,
+                ships: parseInt(ships)
+            });
+        }
+    } while (result);
+
+    return divisions;
+
+}
+
+export { distance, splitPlanets, splitDivisions };

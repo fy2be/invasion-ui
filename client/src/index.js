c37740f6ef44b57490de6d3d236cfa336de008c4
fy2be <36625528+fy2be@users.noreply.github.com>
2018-06-14 19:54:09 +0200

    remove AppMock

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import AppMock from './components/AppMock';
 import './css/style.css';
 
-ReactDOM.render(<AppMock />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
